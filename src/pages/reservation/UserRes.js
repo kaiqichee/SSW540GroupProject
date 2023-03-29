4008c58dc9a908c6ddf09b467cb254126a8cefc9
kqchee <kaiqichee@gmail.com>
2023-03-29 09:59:00 -0400

    add check so only reservations in the future can be edited
    -grey out/disable edit icon when event is past

--- src/pages/reservation/UserRes.js
+++ src/pages/reservation/UserRes.js
@@ -34,10 +34,14 @@ export function UserRes() {
             setShow(true);
         }
 
-        function sendToEdit(id) {
-            localStorage.setItem("currentId", id)
-            window.location.href = '/res/edit';
-            return;
+        function sendToEdit(id, editable) {
+            if (editable){
+                localStorage.setItem("currentId", id)
+                window.location.href = '/res/edit';
+                return;
+            } else {
+                return false;
+            }
         }
 
         async function deleteRes(){
@@ -60,6 +64,14 @@ export function UserRes() {
         }
 
         function tableRow(res, i){
+            let editable = true
+            let editColor = "currentColor"
+
+            let currentDate = new Date()
+            if (new Date(res.startTime)<=currentDate) {
+                editable = false
+                editColor = "#BEBEBE"
+            }
             let confirmed =   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-check-circle" viewBox="0 0 16 16">
             <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
             <path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z"/>
@@ -74,7 +86,7 @@ export function UserRes() {
             <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"/>
             </svg>
 
-            let editIcon = <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pencil" viewBox="0 0 16 16">
+            let editIcon = <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill={editColor} className="bi bi-pencil" viewBox="0 0 16 16">
             <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
             </svg>
 
@@ -87,7 +99,7 @@ export function UserRes() {
                     <td>
                       {res.confirmed ? confirmed : notConfirmed}
                     </td>
-                    <td><a onClick={() => sendToEdit(res._id)}>{editIcon}</a></td>
+                    <td><a onClick={() => sendToEdit(res._id, editable)}>{editIcon}</a></td>
                     <td><a onClick={() => handleShow(res._id)}>{rubbishIcon}</a></td>
 
                 </tr>
